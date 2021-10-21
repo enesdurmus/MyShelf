4d6d6eb91773ca71fb55af83ab9b6c826c218af2
Enes <enes.durmus@stu.fsm.edu.tr>
2021-10-21 21:52:05 +0300

    firestore operations added to createAnAccount page

--- app/src/main/java/com/example/myshelf/CreateAccountActivity.kt
+++ app/src/main/java/com/example/myshelf/CreateAccountActivity.kt
@@ -2,17 +2,42 @@ package com.example.myshelf
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 
 class CreateAccountActivity : AppCompatActivity() {
+
+    private lateinit var _storageHandler : StorageHandler
+    private lateinit var _buttonCreateAccount : Button
+    private lateinit var _textUserName : EditText
+    private lateinit var _textFullName : EditText
+    private lateinit var _eMail : EditText
+    private lateinit var _password : EditText
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_account)
 
         val message = intent.getStringExtra(CreateAccountActivityKey)
 
-        val textView = findViewById<TextView>(R.id.textUserNameCa).apply {
-            text = message
+        _buttonCreateAccount = findViewById(R.id.buttonCreateAnAccountCa)
+        _textUserName = findViewById(R.id.textUserNameCa)
+        _textFullName = findViewById(R.id.textFullNameCa)
+        _eMail = findViewById(R.id.textEmailCa)
+        _password = findViewById(R.id.editTextPasswordCa)
+
+        _storageHandler = StorageHandler("sea")
+
+        _buttonCreateAccount.setOnClickListener{
+            val user: HashMap<String, Any> = HashMap()
+            user["UserName"] = _textUserName.text.toString()
+            user["FullName"] = _textFullName.text.toString()
+            user["E-mail"] = _eMail.text.toString()
+            user["Password"] = _password.text.toString()
+
+            _storageHandler.WriteDataToFirebase(user)
         }
     }
 }
--- app/src/main/java/com/example/myshelf/User.kt
+++ app/src/main/java/com/example/myshelf/User.kt
@@ -5,15 +5,19 @@ package com.example.myshelf
  * @author enesdurmus
  */
 
-class User(userName : String, password : String) {
+class User(userName : String, fullName : String, eMail : String,password : String) {
 
     private var _userName : String
+    private var _fullName : String
+    private var _eMail : String
     private var _password : String
     private var _isRememberMeChecked : Boolean
     private var _firebaseDocumentId : String
 
     init {
         _userName = userName
+        _fullName = fullName
+        _eMail = eMail
         _password = password
         _isRememberMeChecked = false
         _firebaseDocumentId = ""
